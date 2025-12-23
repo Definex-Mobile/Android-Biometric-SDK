2f8e252759242843f45e41c96659635f6fed816a
okuscuoglu <onur.kuscuoglu@teamdefinex.com>
2025-12-23 14:29:37 +0300

    Bump version to 1.0.4 for release

--- biometric-security-sdk/build.gradle.kts
+++ biometric-security-sdk/build.gradle.kts
@@ -65,7 +65,7 @@ afterEvaluate {
                 
                 groupId = "com.github.Definex-Mobile"
                 artifactId = "biometric-security-sdk"
-                version = "1.0.3"
+                version = "1.0.4"
                 
                 pom {
                     name.set("Biometric Security SDK")
